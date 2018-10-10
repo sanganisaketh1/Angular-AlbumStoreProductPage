53a5ddfdc97480eaef295cb938c3b4107324a7ed
sanganisaketh1 <43859781+sanganisaketh1@users.noreply.github.com>
2018-10-10 10:42:40 -0400

    service7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {Http} from '@angular/http';
+import {Http, response} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
